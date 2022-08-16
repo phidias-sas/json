985f467832a825245a482579dfb0aa9865fe0234
Santiago <santiago.cortes@phidias.com.co>
2022-08-16 20:39:48 +0200

    fix typo

--- src/JsonDb/Utils.php
+++ src/JsonDb/Utils.php
@@ -84,7 +84,7 @@ class Utils
             $escaped = preg_replace('~[\x00\x0A\x0D\x1A\x22\x27\x5C]~u', '\\\$0', $string);
         }
 
-        return "'$escaped'";
+        return $escaped;
     }
 
     public static function escape($string)
